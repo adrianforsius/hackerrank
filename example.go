21f8849713bc95e7dbbd32395b8360739bb2bdda
Adrian Forsius <adrianforsius@gmail.com>
2020-04-30 09:42:03 -0500

    Add example

---
+++ example.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Complete the jumpingOnClouds function below.
+func jumpingOnClouds(c []int32) int32 {
+	return 0
+}
+
+func main() {
+	jumps := jumpingOnClouds([]int32{0, 0, 1, 0, 0, 1, 0})
+	fmt.Println("jumps", jumps)
+}
