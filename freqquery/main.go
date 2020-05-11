4cb4c73bdf2b3fe11c2ff36ae33003fde565240f
Adrian Forsius <adrianforsius@gmail.com>
2020-05-10 22:32:10 -0500

    Convert defaultdict python solution to go

--- freqquery/main.go
+++ freqquery/main.go
@@ -9,70 +9,64 @@ func freqQuery(queries [][]int32) []int32 {
 	// Loop over each action and change the structure accordingly
 	// This looks like events could we possibly check if two events cancel each other out?
 	// at each print there is a break, how about we split the query into parts of printing
-	//var orig [][]int32
-	//copy(orig, queries)
 
-	split := make([][][]int32, 0)
-	split = append(split, make([][]int32, 0))
-	index := 0
-	for i, item := range queries {
-		split[index] = append(split[index], item)
-		// remove last iteration if last iteration doesn't end with print
-		if len(queries)-1 == i {
-			if !isPrint(item) {
-				split = split[:len(split)-1]
-			}
-			continue
-		}
-		if isPrint(item) {
-			index++
-			split = append(split, make([][]int32, 0))
-		}
-	}
+	statusMap := make(map[int32]int32, 0)
 	insertMap := make(map[int32]int32, 0)
 	result := make([]int32, 0)
-	for _, stack := range split {
-		for _, part := range stack {
-			if isInsert(part) {
-				if _, ok := insertMap[part[1]]; !ok {
-					insertMap[part[1]] = 1
+	for _, part := range queries {
+		action, value := part[0], part[1]
+		if isInsert(action) {
+			if _, ok := statusMap[insertMap[value]]; !ok {
+				statusMap[insertMap[value]] = 1
+			} else if statusMap[insertMap[value]] > 0 {
+				statusMap[insertMap[value]] -= 1
+			}
+			if _, ok := insertMap[value]; !ok {
+				insertMap[value] = 1
+			} else {
+				insertMap[value] += 1
+			}
+			if _, ok := statusMap[insertMap[value]]; !ok {
+				statusMap[insertMap[value]] = 1
+			} else {
+				statusMap[insertMap[value]] += 1
+			}
+			statusMap[statusMap[insertMap[value]]] += 1
+		} else if isDelete(action) {
+			if statusMap[insertMap[value]] > 0 {
+				statusMap[insertMap[value]] -= 1
+			}
+			if _, ok := insertMap[value]; !ok {
+				insertMap[value] = 1
+			} else if insertMap[value] > 0 {
+				insertMap[value] -= 1
+			}
+			if insertMap[value] > 0 {
+				statusMap[insertMap[value]] += 1
+			}
+		} else if isPrint(action) {
+			if val, ok := statusMap[value]; !ok {
+				result = append(result, 0)
+			} else {
+				if val > 0 {
+					result = append(result, 1)
 				} else {
-					insertMap[part[1]] += 1
+					result = append(result, 0)
 				}
 			}
-			if isDelete(part) {
-				if _, ok := insertMap[part[1]]; ok {
-					insertMap[part[1]] -= 1
-				}
-			}
-		}
-		check := false
-		for _, entry := range insertMap {
-			if entry <= 0 {
-				continue
-			}
-			if entry == stack[len(stack)-1][1] {
-				check = true
-				break
-			}
-		}
-		if check == false {
-			result = append(result, 0)
-		} else {
-			result = append(result, 1)
 		}
 	}
 	return result
 }
 
-func isInsert(s []int32) bool {
-	return s[0] == 1
+func isInsert(s int32) bool {
+	return s == 1
 }
-func isDelete(s []int32) bool {
-	return s[0] == 2
+func isDelete(s int32) bool {
+	return s == 2
 }
-func isPrint(s []int32) bool {
-	return s[0] == 3
+func isPrint(s int32) bool {
+	return s == 3
 }
 
 func main() {
