b4b844d045e074a22c866b94f0ec61b14619f3aa
Adrian Forsius <adrianforsius@gmail.com>
2020-05-15 21:18:43 -0500

    Add bubblesort

---
+++ bubble/main.go
@@ -0,0 +1,11 @@
+package main
+
+// Complete the countSwaps function below.
+func countSwaps(a []int32) {
+
+}
+
+func main() {
+	countSwaps([]int32{0, 0, 1, 0, 0, 1, 0})
+	//fmt.Println("jumps", jumps)
+}
